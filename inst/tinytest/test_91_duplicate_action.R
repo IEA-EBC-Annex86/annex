58837b35afa60b24c16da852c5bc02e284ba2ee3
Reto Stauffer <reto.stauffer@uibk.ac.at>
2023-12-22 07:08:42 +0100

    fix: Adjusted tests to the new system where we need (N-NAs >= 30)

--- inst/tinytest/test_91_duplicate_action.R
+++ inst/tinytest/test_91_duplicate_action.R
@@ -10,43 +10,77 @@ if (interactive()) {
 # added in annex version 0.2-9+
 
 # Setting up the data set (prepared df; manually)
-# Note: We need at least 10 valid observations to be able to calculate
+# Note: We need at least 30 valid observations to be able to calculate
 # the stats, thus the latter one are used to 'fill up' the data.
 data <- "
 datetime;         study; home; room; CO2; T;      note
-2023-11-03 08:00; test;  test; BED1;  500;  25.5;  08:00 has no duplicate
-2023-11-03 08:00; test;  test; BED2;  400;  18.2;  08:00 has no duplicate
-2023-11-03 09:00; test;  test; BED1;  520;  24.8;  ---
-2023-11-03 09:00; test;  test; BED2;  434;  18.1;  ---
-2023-11-03 09:00; test;  test; BED1;   NA;    NA;  duplicated 09:00, all missing
-2023-11-03 09:00; test;  test; BED2;   NA;    NA;  duplicated 09:00, all missing
-2023-11-03 10:00; test;  test; BED1;  540;  24.7;  ---
-2023-11-03 10:00; test;  test; BED2;  427;  18.0;  ---
-2023-11-03 10:00; test;  test; BED1;   NA;  24.7;  duplicated 10:00, partially missing
-2023-11-03 10:00; test;  test; BED2;  427;    NA;  duplicated 10:00, partially missing
-2023-11-03 11:00; test;  test; BED1;  535;  25.1;  ---
-2023-11-03 11:00; test;  test; BED2;  413;  18.3;  ---
-2023-11-03 11:00; test;  test; BED1; -535; -25.1;  duplicated 11:00, different values (neg)
-2023-11-03 11:00; test;  test; BED2; -413; -18.3;  duplicated 11:00, different values (neg)
+2023-11-03 08:05; test;  test; BED1;  500;  25.5;  08:05 has no duplicate
+2023-11-03 08:05; test;  test; BED2;  400;  18.2;  08:05 has no duplicate
+2023-11-03 08:10; test;  test; BED1;  520;  24.8;  ---
+2023-11-03 08:10; test;  test; BED2;  434;  18.1;  ---
+2023-11-03 08:10; test;  test; BED1;   NA;    NA;  duplicated 08:10, all missing
+2023-11-03 08:10; test;  test; BED2;   NA;    NA;  duplicated 08:10, all missing
+2023-11-03 08:20; test;  test; BED1;  540;  24.7;  ---
+2023-11-03 08:20; test;  test; BED2;  427;  18.0;  ---
+2023-11-03 08:20; test;  test; BED1;   NA;  24.7;  duplicated 08:20, partially missing
+2023-11-03 08:20; test;  test; BED2;  427;    NA;  duplicated 08:20, partially missing
+2023-11-03 08:30; test;  test; BED1;  535;  25.1;  ---
+2023-11-03 08:30; test;  test; BED2;  413;  18.3;  ---
+2023-11-03 08:30; test;  test; BED1; -535; -25.1;  duplicated 08:30, different values (neg)
+2023-11-03 08:30; test;  test; BED2; -413; -18.3;  duplicated 08:30, different values (neg)
 ### Valid data to fill up the data.frame
+2023-11-03 09:00; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:00; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 09:10; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:10; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 09:20; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:20; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 09:30; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:30; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 09:40; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:40; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 09:50; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 09:50; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:00; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:00; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:10; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:10; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:20; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:20; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:30; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:30; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:40; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:40; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 10:50; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 10:50; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:00; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:00; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:10; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:10; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:20; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:20; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:30; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:30; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:40; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:40; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 11:50; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 11:50; test;  test; BED2;  401;  19.1;  filler
 2023-11-03 12:00; test;  test; BED1;  501;  24.1;  filler
 2023-11-03 12:00; test;  test; BED2;  401;  19.1;  filler
-2023-11-03 13:00; test;  test; BED1;  502;  24.2;  filler
-2023-11-03 13:00; test;  test; BED2;  402;  19.2;  filler
-2023-11-03 14:00; test;  test; BED1;  503;  24.3;  filler
-2023-11-03 14:00; test;  test; BED2;  403;  19.3;  filler
-2023-11-03 15:00; test;  test; BED1;  504;  24.4;  filler
-2023-11-03 15:00; test;  test; BED2;  404;  19.4;  filler
-2023-11-03 16:00; test;  test; BED1;  505;  24.5;  filler
-2023-11-03 16:00; test;  test; BED2;  405;  19.5;  filler
-2023-11-03 17:00; test;  test; BED1;  506;  24.6;  filler
-2023-11-03 17:00; test;  test; BED2;  406;  19.6;  filler
-2023-11-03 18:00; test;  test; BED1;  507;  24.7;  filler
-2023-11-03 18:00; test;  test; BED2;  407;  19.7;  filler
-2023-11-03 19:00; test;  test; BED1;  508;  24.8;  filler
-2023-11-03 19:00; test;  test; BED2;  408;  19.8;  filler
-2023-11-03 20:00; test;  test; BED1;  509;  24.9;  filler
-2023-11-03 20:00; test;  test; BED2;  409;  19.9;  filler
+2023-11-03 12:10; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 12:10; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 12:20; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 12:20; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 12:30; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 12:30; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 12:40; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 12:40; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 12:50; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 12:50; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 13:00; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 13:00; test;  test; BED2;  401;  19.1;  filler
+2023-11-03 13:10; test;  test; BED1;  501;  24.1;  filler
+2023-11-03 13:10; test;  test; BED2;  401;  19.1;  filler
 "
 
 data <- read.csv(text = data, sep = ";", header = TRUE, strip.white = TRUE, comment.char = "#")
@@ -83,8 +117,8 @@ expect_true("stats" %in% ls()) # Make sure object has been created
 
 
 expect_identical(dim(stats), c(12L, 127L), info = "Statistics dimension check")
-expect_true(all(stats$N == 16),
-            info = "Check if N == 16 (counts duplicates)")
+expect_true(all(stats$N == 33),
+            info = "Check if N == 33 (counts duplicates)")
 expect_true(all(stats$NAs == 1 | stats$NAs == 2),
             info = "Check count for number of duplicates")
 
@@ -133,7 +167,7 @@ expect_true(all(stats$p00 < 0),
 
 # In addition, we should now always have N = 4 and NAs = 0 as
 # we were able to remove all missing values.
-expect_true(all(stats$N   == 13), info = "Sample size after aggregating duplicates")
+expect_true(all(stats$N   == 30), info = "Sample size after aggregating duplicates")
 expect_true(all(stats$NAs ==  0), info = "Missing values after aggregating duplicates")
 
 rm(annex_df)
