6e30af7067e8591cbfbd0ac70e6fe26d90050bcf
Reto Stauffer <reto.stauffer@uibk.ac.at>
2023-12-22 07:09:16 +0100

    fix/feat: New tests for small sample sizes; cases where we have <30 or ==1 valid observations for the stats

--- inst/tinytest/test_92_smallN.R
+++ inst/tinytest/test_92_smallN.R
@@ -17,7 +17,7 @@ if (interactive()) {
 }
 
 
-# Creating a simulated data set w/ 10-min observations (Europe/Berlin time
+# Creating a simulated data set w/ 5-min observations (Europe/Berlin time
 # (CET)) for a series of rooms with some artificial temperature observations
 # for three rooms; from all observations being available to 
 # all missing in different proportions.
@@ -25,10 +25,10 @@ if (interactive()) {
 # All observations from 10:00 local time to 14:00 local time (only during daytime).
 #
 # Room LIV1: All data available (48 observations per day)
-# Room LIV2: Only 10 observations per day.
-# Room LIV3: Only 10 valid observations per day.
-# Room LIV4: Only  9 observations per day.
-# Room LIV5: Only  9 valid observations per day.
+# Room LIV2: Only 30 observations per day.
+# Room LIV3: Only 30 valid observations per day.
+# Room LIV4: Only 29 observations per day.
+# Room LIV5: Only 29 valid observations per day.
 # Room LIV6: Only  1 observations per day.
 # Room LIV7: Only  1 valid observations per day.
 # Room LIV8: Only  0 valid observations per day.
@@ -36,8 +36,8 @@ if (interactive()) {
 # This is the 'prepared df' format which can directly be used with annex().
 times <- seq(from = as.POSIXct("2023-01-01 10:00:00", tz = "Europe/Berlin"),
              to   = as.POSIXct("2023-01-01 15:00:00", tz = "Europe/Berlin"),
-             by   = 10 * 60)
-expect_equal(length(times), 31L,
+             by   = 5 * 60)
+expect_equal(length(times), 61L,
              info = "Checking length of simulated time data set")
 
 # Helper function
@@ -69,10 +69,10 @@ fn <- function(times, room, N, na.rm) {
 # Configuration for simulating the data
 cnf <- "room,N,na.rm
 LIV1, 999, FALSE
-LIV2,  10, TRUE
-LIV3,  10, FALSE
-LIV4,   9, TRUE
-LIV5,   9, FALSE
+LIV2,  30, TRUE
+LIV3,  30, FALSE
+LIV4,  29, TRUE
+LIV5,  29, FALSE
 LIV6,   1, TRUE
 LIV7,   1, FALSE
 LIV8,   0, FALSE"
@@ -86,12 +86,11 @@ data <- do.call(rbind, data)
 # Testing my own data set
 tmp <- split(data, data$room)
 expect_equivalent(sapply(tmp, function(y) sum(!is.na(y$T))),
-                  c(31, 10, 10, 9, 9, 1, 1, 0),
+                  c(61, 30, 30, 29, 29, 1, 1, 0),
                   info = "Checking number of non-missing values in demo data set")
 expect_equivalent(sapply(tmp, function(y) sum(is.na(y$T))),
-                  c(0, 0, 21, 0, 22, 0, 30, 31),
+                  c(0, 0, 31, 0, 32, 0, 60, 61),
                   info = "Checking number of missing values in demo data set")
-
 expect_identical(attr(data$datetime, "tzone"), "Europe/Berlin",
                  info = "Checking time zone of demo data set")
 
@@ -103,8 +102,12 @@ expect_silent(prepared_df <- annex(T ~ datetime | study + home + room, data = da
               info = "Prepare annex data.frame")
 expect_identical(nrow(prepared_df), nrow(data),
                  info = "Checking dimension of prepared annex df; should be the same as 'data'")
+
+# Calculating stats
+stats <- annex_stats(prepared_df)
 expect_silent(stats <- annex_stats(prepared_df),
               info = "Calculating annex statistics")
+
 
 # -------------------------------------------------------------------
 # Prepare the data set and calculate the statistics
@@ -113,15 +116,25 @@ expect_silent(stats <- annex_stats(prepared_df),
 # Check: if N - NAs < 10 we should not have certain statistics. This
 # includes 'Mean', 'Sd', and all percentiles, and we also removed
 # the guessed intervals (as this guess might be way off) including 'Nestim'.
-expect_identical(length(idx <- which(stats$N - stats$NAs < 10)), 12L,
+expect_identical(length(idx <- which(stats$N - stats$NAs < annex:::minsamplesize)), 12L,
                  info = "Find rows where N - NAs < 10")
 
-# Find 'inveral_*', 'Nestim', 'Mean', 'Sd' and all 'p*' cols.
-cols <- names(stats)[grepl("^(interval_.*|Nestim|Mean|Sd|p[0-9\\.]+)$", names(stats))]
-expect_identical(length(cols), 114L,
-                 info = "Identify columns which should be NA")
+# If N - NAs is lower than annex:::minsamplesize, Mean and Sd must be NA
+idx <- which(stats$N - stats$NAs < annex:::minsamplesize)
+expect_identical(idx, as.integer(c(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21)),
+                 info = "Rows/entries where N - NAs is 'small' (smaller than annex:::minsamplesize)")
+# Check that Mean and Sd is missing
+expect_true(all(is.na(stats$Mean[idx])),
+            info = "Checking Mean == NA if N - NAs too small")
+expect_true(all(is.na(stats$Sd[idx])),
+            info = "Checking Sd == NA if N - NAs too small")
+
+# If N - NAs is == 1 we can't guess an intervall. Check that all interval_* entries
+# as well as Nestim is NA.
+cols <- names(stats)[grepl("^(interval_.*|Nestim)$", names(stats))]
+idx  <- which(stats$N - stats$NAs == 1)
 expect_true(all(is.na(stats[idx, cols])),
-            info = "Check if the cells which should be NA are indeed NA")
+            info = "Checking interval_* and Nestim == NA if N - NAs == 1")
 
 # -------------------------------------------------------------------
 # Save to stats file
@@ -131,6 +144,50 @@ expect_silent(annex_write_stats(stats, tmpfile, user = 999),
               info = "Writing statistics to file")
 expect_true(annex:::annex_validate_sheet_STAT(tmpfile, user = 999, quiet = TRUE),
             info = "Testing XLSX sheet 'STAT' (only this one)")
+
+# -------------------------------------------------------------------
+# Testing error checks. Therefore we make a copy of the xlsx file
+# and manually manipulate some of the entries in the sheet 'STAT'
+# to see if annex_validate_sheet_STAT() finds them.
+# -------------------------------------------------------------------
+require("openxlsx")
+tmpfile2 <- tempfile(fileext = ".xlsx")
+
+savewb <- function(wb, dst, data) {
+    writeData(wb, sheet = "STAT", x = data, colNames = FALSE, startRow = 2, startCol = 1)
+    saveWorkbook(wb, dst, overwrite = TRUE)
+}
+
+# Setting Mean of first data row to NA (must not be NA)
+wb  <- loadWorkbook(tmpfile)
+tmp <- readWorkbook(wb, sheet = "STAT")
+tmp$Mean[1] <- NA
+savewb(wb, tmpfile2, tmp)
+expect_error(annex:::annex_validate_sheet_STAT(tmpfile2, user = 999, quiet = TRUE),
+             pattern = ".*missing values.*in column 'Mean'.*",
+             info = "Missing 'Mean' altough N - NAs > required minimum")
+
+# Setting Sd of first data row to NA (must not be NA)
+wb  <- loadWorkbook(tmpfile)
+tmp <- readWorkbook(wb, sheet = "STAT")
+tmp$Sd[1] <- NA
+savewb(wb, tmpfile2, tmp)
+expect_error(annex:::annex_validate_sheet_STAT(tmpfile2, user = 999, quiet = TRUE),
+             pattern = ".*missing values.*in column 'Sd'.*",
+             info = "Missing 'Mean' altough N - NAs > required minimum")
+
+# Same if Sd is negative
+wb  <- loadWorkbook(tmpfile)
+tmp <- readWorkbook(wb, sheet = "STAT")
+tmp$Sd[1] <- -1
+savewb(wb, tmpfile2, tmp)
+expect_error(annex:::annex_validate_sheet_STAT(tmpfile2, user = 999, quiet = TRUE),
+             pattern = ".*missing values.*in column 'Sd'.*",
+             info = "Missing 'Mean' altough N - NAs > required minimum")
+
+
+
+
 
 
 
